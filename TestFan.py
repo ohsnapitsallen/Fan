c1dca0deac07e3f97f8055cf375d0b977659781e
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 13:57:41 +0800

    Added print statements to method

--- TestFan.py
+++ TestFan.py
@@ -8,4 +8,15 @@ class TestFan:
         fan1 = Fan(Fan.FAST, 10.00, 'yellow', True)
         fan2 = Fan(Fan.MEDIUM, 5.00, 'blue', False)
 #Print the properties given
+        print("TestFan 1:")
+        print("Fan Speed:", fan1.get_fanspeed())
+        print("Fan Radius:", fan1.get_fanradius())
+        print("Fan Color:", fan1.get_fancolor())
+        print("Turned On?:", fan1.get_fanon())
+
+        print("\nTestFan 2:")
+        print("Fan Speed:", fan2.get_fanspeed())
+        print("Fan Radius:", fan2.get_fanradius())
+        print("Fan Color:", fan2.get_fancolor())
+        print("Turned On?:", fan2.get_fanon())
 #Start the test program
