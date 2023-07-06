eb5cfb4b82ecb31f833bc9447955a646a233a1ff
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 11:44:29 +0800

    Added constants

--- Fan.py
+++ Fan.py
@@ -1,6 +1,9 @@
 #Create a class named fan
 class Fan:
 #Create three constants to represent a fan
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
 #Set the default values    
 #Create getter methods
 #Create setter methods
