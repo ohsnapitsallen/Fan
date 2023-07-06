9e9c0ecc73b912e392a508b758f781bdbc6a05ff
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 13:15:00 +0800

    Constructor Added

--- Fan.py
+++ Fan.py
@@ -5,5 +5,10 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 #Set the default values    
+    def __init__(self, fanspeed = SLOW, fanradius = 5.00, fancolor = 'blue', fanon=False):
+        self.__fanspeed = fanspeed
+        self.__fanon = fanon
+        self.__fanradius = fanradius
+        self.__fancolor = fancolor
 #Create getter methods
 #Create setter methods
