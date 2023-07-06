123513995b47863dc6bb2958b471ca1919d92e9b
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 13:51:13 +0800

    2 Test fans added

--- TestFan.py
+++ TestFan.py
@@ -4,5 +4,8 @@ from Fan import*
 #Create a class to test the program
 class TestFan:
 #Create 2 test variables with the given properties
+    def main():
+        fan1 = Fan(Fan.FAST, 10.00, 'yellow', True)
+        fan2 = Fan(Fan.MEDIUM, 5.00, 'blue', False)
 #Print the properties given
 #Start the test program
